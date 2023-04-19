06573f4974df54ea142c3ed34bb1e96a74df1201
Charlotte <cmparent@umich.edu>
2023-04-19 19:53:53 -0400

    changes

--- weather.py
+++ weather.py
@@ -8,29 +8,46 @@ import requests
 def get_data_weather(city):
     api_url = 'https://api.api-ninjas.com/v1/weather?city={}'.format(city)
     response = requests.get(api_url, headers={'X-Api-Key': 'ClqOtbxYh1QnlxYsIcDbWQ==6wVFffdvVGFwF4OI'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
-    else:
-        print("Error:", response.status_code, response.text)
+    
+    data = json.loads(response.text)
+    # print(data)
+    
+    # if response.status_code == requests.codes.ok:
+    #     print(response.text)
+    # else:
+        # print("Error:", response.status_code, response.text)
 
 
 def create_weather_table(cities, cur, conn):
     
-    cur.execute("DROP TABLE IF EXISTS weather")
+    # cur.execute("DROP TABLE IF EXISTS weather")
     cur.execute("CREATE TABLE IF NOT EXISTS weather (ID INTEGER PRIMARY KEY AUTOINCREMENT, country_ID INTEGER FOREIGN KEY, air_quality_ID INTEGER FOREIGN KEY, wind_speed INTEGER, wind_degrees INTEGER, temperature INTEGER, humidity INTEGER, sunrise INTEGER, sunset INTEGER, cloud_pct INTEGER, feels_like INTEGER, max_temp INTEGER, min_temp INTEGER)") 
 
-    # cur.execute('SELECT 
+    first = cur.fetchone()
 
+    if first == None:
+        first = 0
+    first = first[0] + 1
 
-    count = 0
+    for city in cities:
+        print(city)
 
 
 
 
 def main():
 
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/airports.db')
+    cur = conn.cursor()
+
     cities = ['Detroit', 'Paris', 'Barcelona', 'Lisbon', 'Berlin', 'Brussel', 'Shanghai', 'Tokyo', 'Seoul', 'Bangkok', 'Rome', 'Dublin', 'London', 'Toronto', 'Mexico City', 'Rio de Janeiro', 'Sydney', 'Amsterdam', 'Copenhagen', 'Havana', 'Dubai', 'Vienna', 'Wellington', 'Prague', 'Jerusalem', 'Lima', "Abuja", "Accra", "Addis Ababa", "Algiers", "Amman", "Ankara", "Ashgabat", "Asmara", "Astana", "Athens", "Baku", "Bamako", "Bandar Seri Begawan", "Bangui", "Banjul", "Bishkek", "Bissau", "Bogotá", "Monrovia", "Male", "Bridgetown", "Brussels", "Bucharest", "Buenos Aires", "Bujumbura", "Cairo", "Canberra", "Caracas", "Castries", "Chisinau", "Colombo", "Conarky", "Maseru", "Dakar", "Damascus", "Dar es Salaam", "Dhaka", "Djibouti City", "Monaco", "Doha", "Dublin", "Dushanbe", "Freetown", "Funafuti", "Gaborone", "Georgetown", "Guatemala City", "Hanoi", "Harare", "Honiara", "Islamabad", "Jakarta", "Kabul", "Kampala", "Kathmandu", "Khartoum", "Kiev", "Kigali", "Kingston", "Kingstown", "Kinshasa", "Kuala Lummpur", "Kuwait City", "La Paz", "Libreville", "Lilognwe", "Pyongyang", "Lomé", "Mayen", "Longyearbyen", "Luanda", "Lusaka", "Luxembourg City", "Zagreb"]
     # print(len(cities))
+
+    create_weather_table(cities, cur, conn)
+
+    # for city in create_weather_tablecities:
+    #     get_data_weacreate_weather_tablether(city)
 
 main()
 
