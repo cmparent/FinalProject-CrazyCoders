8bf3533152cb63fe1061ea63eb7777f0b8cab03b
Charlotte <cmparent@umich.edu>
2023-04-18 21:14:43 -0400

    EDITED Changes

--- finalproject.py
+++ finalproject.py
@@ -17,49 +17,5 @@ cur.execute("CREATE TABLE IF NOT EXISTS weather (ID INTEGER, country_ID INTEGER,
 cur.execute("CREATE TABLE IF NOT EXISTS country (ID INTEGER, gdp INTEGER, surface_area INTEGER, life_expectancy_male INTEGER, imports INTERGER, currency_name TEXT, urban_population_growth INTEGER, capital TEXT, co2_emissions INTEGER, tourists INTEGER, life_expectancy_female INTEGER, population INTEGER, urban_population INTEGER, name TEXT, pop_growth INTEGER, region INTEGER, pop_density INTEGER, refugees INTEGER)")
 cur.execute("CREATE TABLE IF NOT EXISTS air_quality (ID INTEGER, AQI INTEGER, CO INTEGER, PM10 INTEGER, SO2 INTEGER, PM25 INTEGER, O3 INTEGER, NO2 INTEGER)")
 
-# weather API - get data
 
-# cities = []
-
-# url = "https://api.api-ninjas.com/v1/weather?country={}".format(country)
-# # api_key = 
-# response = requests.get(url) #, headers = {'X-Api-Key': 'ClqOtbxYh1QnlxYsIcDbWQ==6wVFffdvVGFwF4OI'})
-# print(response)
-
-# # country API
-
-
-
-url1 = "'https://api.api-ninjas.com/v1/country?name=United States'
-response1 = requests.get(url, headers = {'X-Api-Key': 'KXNSpXxVeBlWPPAuIjI3Cg==7Oe7vdmd05QCcjZ7'})
-
-if response1.status_code == requests.codes.ok:
-    print(response1.text)
-else:
-    print("Error:", response1.status_code, response.text)
-
-# airport API
-airport_country = "US"
-airport_url = "https://api.api-ninjas.com/v1/airports?country={}".format(airport_country)
-resp = requests.get(airport_url, headers = {'X-Api-Key': 'ClqOtbxYh1QnlxYsIcDbWQ==6wVFffdvVGFwF4OI'})
-
-if resp.status_code == requests.codes.ok:
-    print(resp.text)
-else:
-    print("Error:", resp.status_code, resp.text)
-
-# print(resp.tecleart.split())
-
-
-# resp = requests.get(airport_url, headers )
-# print(resp.text)
-
-
-
-airport_url = 'https://api.api-ninjas.com/v1/airports?name={}'.format(country)
-airport_response = requests.get(airport_url, headers = {'X-Api-Key': 'UlC4xXn/1yAqzYBvCS/Wkg==EJ2pSOpCn3TEU3iQ'})
-if airport_response.status_code == requests.codes.ok:
-    print(airport_response.text)
-else:
-    print("Error:", airport_response.status_code, airport_response.text)
 
