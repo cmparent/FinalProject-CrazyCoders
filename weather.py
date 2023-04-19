0e1411ebfb1119d4dd860769b614567f993bc949
Charlotte <cmparent@umich.edu>
2023-04-18 21:58:52 -0400

    Finish weather function

--- weather.py
+++ weather.py
@@ -1,1 +1,22 @@
+import unittest
+import json
+import os
+import sqlite3
+import requests
+
+
+def get_data_weather(city):
+    api_url = 'https://api.api-ninjas.com/v1/weather?city={}'.format(city)
+    response = requests.get(api_url, headers={'X-Api-Key': 'ClqOtbxYh1QnlxYsIcDbWQ==6wVFffdvVGFwF4OI'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
+
+
+def make_weather_table(cities, cur, conn):
+    
+
+
+
 
