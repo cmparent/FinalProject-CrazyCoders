0ebd76d4684e8f434f51b4b21d7f35df916fc91c
serenashen1 <102195030+serenashen1@users.noreply.github.com>
2023-04-18 21:44:47 -0400

    1

--- airport.py
+++ airport.py
@@ -1,1 +1,23 @@
+import unittest
+import sqlite3
+import json
+import os
+import requests
+
+def get_data(city):
+
+    airport_url = 'https://api.api-ninjas.com/v1/airports?name={}'.format(city)
+    airport_response = requests.get(airport_url, headers = {'X-Api-Key': 'UlC4xXn/1yAqzYBvCS/Wkg==EJ2pSOpCn3TEU3iQ'})
+    
+    if airport_response.status_code == requests.codes.ok:
+        print(airport_response.text)
+    else:
+        print("Error:", airport_response.status_code, airport_response.text)
+
+
+def create_table(cities, cur, conn):
+
+    cur.execute('CREATE TABLE IF NOT EXISTS airports (ID INTEGER, ICAO_code INTEGER, IATA_code INTEGER, name TEXT, location_ID INTEGER)')
+    
+    cur.execute("CREATE TABLE IF NOT EXISTS airport_locations (ID INTEGER, country_ID INTEGER, weather_ID INTEGER, city TEXT, region TEXT, timezone TEXT, latitude INTEGER, longitute INTEGER, elevation INT)")
 
