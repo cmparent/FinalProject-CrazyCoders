2415ae191a83e163156b82a606867f4aa3b13ad7
Charlotte <cmparent@umich.edu>
2023-04-18 22:17:05 -0400

    Changes

--- airport.py
+++ airport.py
@@ -24,3 +24,4 @@ def create_table(cities, cur, conn):
 
 
 cities = ['Detroit', 'Paris', 'Barcelona', 'Lisbon', 'Berlin', 'Brussel', 'Shanghai', 'Tokyo', 'Seoul', 'Bangkok', 'Rome', 'Dublin', 'London', 'Toronto', 'Mexico City', 'Rio de Janeiro', 'Sydney', 'Amsterdam', 'Copenhagen', 'Havana', 'Dubai', 'Vienna', 'Wellington', 'Prague', 'Jerusalem', 'Lima']
+print(len(cities))
