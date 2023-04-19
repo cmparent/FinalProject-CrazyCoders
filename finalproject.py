76274dad5ebc1b93626708296f18021d13db4c04
Charlotte <cmparent@umich.edu>
2023-04-18 20:44:54 -0400

    Deleted airports-journal

--- finalproject.py
+++ finalproject.py
@@ -5,22 +5,22 @@ import sqlite3
 import requests
 
 
-def create_tables(db):
-    path = os.path.dirname(os.path.abspath(__file__))
-    conn = sqlite3.connect(path+'/'+db)
-    cur = conn.cursor()
+# def create_tables(db):
+path = os.path.dirname(os.path.abspath(__file__))
+conn = sqlite3.connect(path+'/'+"airports")
+cur = conn.cursor()
 
-    # cur.execute('DROP TABLE IF EXISTS airports')
-    cur.execute('CREATE TABLE IF NOT EXISTS airports (ID INTEGER, ICAO_code INTEGER, IATA_code INTEGER, name TEXT, location_ID INTEGER)')
-    cur.execute("CREATE TABLE IF NOT EXISTS (")
+#  cur.execute('DROP TABLE IF EXISTS airports')
+cur.execute('CREATE TABLE IF NOT EXISTS airports (ID INTEGER, ICAO_code INTEGER, IATA_code INTEGER, name TEXT, location_ID INTEGER)')
+cur.execute("CREATE TABLE IF NOT EXISTS airport_locations (ID INTEGER, country_ID INTEGER, weather_ID INTEGER, city TEXT, region TEXT, timezone TEXT, latitude INTEGER, longitute INTEGER, elevation INT)")
+cur.execute("CREATE TABLE IF NOT EXISTS weather (ID INTEGER, country_ID INTEGER, air_quality_ID INTEGER, wind_speed INTEGER, wind_degrees INTEGER, temperature INTEGER, humidity INTEGER, sunrise INTEGER, sunset INTEGER, cloud_pct INTEGER, feels_like INTEGER, max_temp INTEGER, min_temp INTEGER)") 
+ 
+
+cur.execute("SELECT * FROM airports")
 
 
 
-    cur.execute("SELECT * FROM airports")
-
-
-
-create_tables("airports")
+# create_tables("airports")
 
 
 
