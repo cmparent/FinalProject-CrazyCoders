f46ce264f2642f5f93e177ab558ce0bb376bd7c8
serenashen1 <102195030+serenashen1@users.noreply.github.com>
2023-04-19 21:24:32 -0400

    new stuff

--- airport.py
+++ airport.py
@@ -15,20 +15,50 @@ def get_data(city):
         print("Error:", airport_response.status_code, airport_response.text)
 
 
-def create_table(cities, cur, conn):
-
-    cur.execute('CREATE TABLE IF NOT EXISTS airports (ID INTEGER PRIMARY KEY AUTOINCREMENT, ICAO_code INTEGER, IATA_code INTEGER, name TEXT, location_ID INTEGER, FOREIGN KEY location_ID REFERENCES airport_locations(ID))')
-
-    cur.execute("CREATE TABLE IF NOT EXISTS airport_locations (ID INTEGER PRIMARY KEY AUTOINCREMENT, country_ID INTEGER, weather_ID INTEGER, city TEXT, region TEXT, timezone TEXT, latitude INTEGER, longitute INTEGER, elevation INT, FOREIGN KEY country_ID REFERENCES country(ID), FOREIGN KEY weather_ID REFERENCES weather(ID))")
 def create_tables(cities, cur, conn):
     cur.execute("DROP TABLE IF EXISTS airports")
-    cur.execute("CREATE TABLE airports (ID INTEGER PRIMARY KEY, ICAO_CODE TEXT NOT NULL, IATA_CODE TEXT NOT NULL, name TEXT NOT NULL)")
+    cur.execute("CREATE TABLE airports (ID INTEGER PRIMARY KEY, ICAO_CODE TEXT NOT NULL, IATA_CODE TEXT NOT NULL, name TEXT NOT NULL, city TEXT NOT NULL)")
     
     cur.execute("DROP TABLE IF EXISTS airport_locations")
     cur.execute("CREATE TABLE airport_locations (ID INTEGER PRIMARY KEY, city TEXT NOT NULL, region TEXT NOT NULL, timezone TEXT, latitude INTEGER NOT NULL, longitute INTEGER NOT NULL, elevation INTERGER NOT NULL)")
-
-
+    
     count = 0
+
+    first = cur.fetchone()
+
+    if (first == None):
+        first = 0
+    else:
+        first = first[0] + 1
+    
+    for city in cities[first: first + 25]:
+        airportdata = get_data(city)
+
+        ID = first + count
+
+
+
+    #     country_id = first + count
+    #     countries_name = country
+
+    #     try:
+    #         aqi = airportdata['overall_aqi']
+    #     except:
+    #         aqi = -1
+
+    #     try:
+    #         ozone = airportdata['O3']['concentration'] 
+    #     except:
+    #         ozone = -1
+
+    #     cur.execute("INSERT OR IGNORE INTO air_quality (country_id,country,air_quality_index,ozone_concentration) VALUES (?,?,?,?)",(country_id,countries_name,aqi,ozone))
+
+    #     count += 1
+
+    # conn.commit()
+
+        
+
 
 
 
@@ -42,7 +72,6 @@ def main():
     countries = ['United States', 'France', 'Spain', 'Portugal', 'Germany', 'Belgium', 'China', 'Japan', 'South Korea', 'Thailand', 'Italy', 'Ireland', 'England', 'Canada','Mexico', 'Brazil', 'Australia', 'Netherlands', 'Denmark', 'Cuba', 'UAE', 'Austria', 'Czech Republic', 'Israel', 'Peru', 'Nigeria', 'Ghana', 'Ethiopia', 'Algeria', 'Jordan', 'Turkey', 'Turkmenistan', 'Etritrea', 'Kazakhstan', 'Greece', 'Azerbaijan', 'Mali', 'Brunei', 'Central African Republic', 'Gambia', 'Kyrgyzstan', 'Guinea-Bissau', 'Colombia', 'Liberia', 'Barbados', 'Maldives', 'Romania', 'Hungary', 'Argentina', 'Burundi', 'Egypt', 'Australia', 'Venezuela', 'Saint Lucia', 'Moldova', 'Sri Lanka', 'Guinea', 'Lesotho', 'Senegal', 'Syria', 'Tanzania', 'Bangladesh', 'Dijbouti', 'Monaco', 'Qatar', 'Ireland', 'Tajikstan', 'Sierra Leone', 'Tuvalu', 'Botswana', 'Guyana', 'Guatemala', 'Vietnam', 'Zimbabwe', 'Finland', 'Soloman Islands', 'Pakistan', 'Indonesia', 'Afghanistan', 'Uganda', 'Nepal', 'Sudan', 'Ukraine', 'Rwanda', 'Jamaica', 'Saint Vincent and the Grendadines', 'Democratic Republic of the Congo', 'Malaysia', 'Kuwait', 'Bolivia', 'Gabon', 'Malawi', 'North Korea', 'Slovenia', 'Togo', 'Svalbard and Jan Mayen', 'Angola', 'Zambia', 'Luxembourg', 'Croatia']
 
     create_tables(cities, cur, conn)
-    # create_locations_table(cities, cur, conn)
 
 
 if __name__ == "__main__":
