def333fbc195daf004ec7aa9f6c2f91f7be8e881
Charlotte <cmparent@umich.edu>
2023-04-21 12:36:02 -0400

    Changes

--- visualizations.py
+++ visualizations.py
@@ -47,7 +47,7 @@ def avg_tourists(cur):
     plt.xlabel("Number of Tourists (more than 200) per Country")
     plt.ylabel("Country Elevation (ft)")
     plt.title('Number of Tourists per Country vs. Country Elevation (ft)')
-    # plt.show()
+    plt.show()
 
     cur.execute("SELECT AVG(c.tourists) , l.elevation, l.city FROM country c JOIN airport_locations l ON c.ID = l.ID WHERE l.elevation >= 1000  GROUP BY l.elevation") 
     high_elevation = cur.fetchall()
